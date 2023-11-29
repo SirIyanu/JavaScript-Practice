b82297056de8422bf2e03c230d37f92e3d624e9c
SirIyanu <105286160+SirIyanu@users.noreply.github.com>
2023-11-29 16:36:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function userBankInfo(name, number, bank){
+const accountDetails={
+    accName: name ,
+    accumber: number ,
+    bank: bank
+ }
+ return accountDetails
+}
+console.log (userBankInfo(
+    "Anu", 81523, "GTBank"
+)
+
+)
+console.log (userBankInfo(
+    "Emem", 97689, "UBA"
+))
